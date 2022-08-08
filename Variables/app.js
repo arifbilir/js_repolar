de520ed166d1cb1871315ac29f8481855979995c
arifbilir <arifbilirde@gmail.com>
2022-08-08 03:10:34 +0200

    variables

---
+++ Variables/app.js
@@ -0,0 +1,27 @@
+// // CONST
+// // TANIMLANAN BİR DEĞER AYNI İSİM İLE TEKRARDAN TANIMLANAMAZ VE DEĞİTİRİLEMEZ
+// const a = 12;
+// console.log(a);
+// // Uncaught SyntaxError: Identifier 'a' has already been declared
+// const a = 12;
+// console.log(a);
+// player.name = "mustafa" olarak değişim yapılabilir.
+// const player = {
+//   name: "Enfa",
+//   age: 16,
+// };
+
+// // LET
+// // TANIMLANAN BİR DEĞER AYNI İSİM İLE TEKRARDAN TANIMLANAMAZ AMA DEĞERİ DEĞİŞTİRİLEBİLİR.
+// let a = 12;
+// console.log(a);
+// // Uncaught SyntaxError: Identifier 'a' has already been declared (at app.js:13:5)
+// let a = 15;
+// console.log(a);
+
+// // VAR
+// // TANIMLANAN BİR DEĞERİ TEKRARDAN TANIMLAYABİLİR VE DEĞERİNİ DEĞİŞTİREBİLİRİZ.
+// var a = 12;
+// console.log(a);
+// var a = 15;
+// console.log(a);
